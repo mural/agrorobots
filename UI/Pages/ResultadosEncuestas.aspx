cfbb7489011387dbfa3eb6427d839db3b35ec7e9
Agustin <mural86@gmail.com>
2015-11-02 03:27:06 -0300

    Resultados de encuestas

--- UI/Pages/ResultadosEncuestas.aspx
+++ UI/Pages/ResultadosEncuestas.aspx
@@ -14,6 +14,41 @@
     <h2>
         <asp:Label ID="lblTitulo_10115" runat="server" Text="titulo"></asp:Label>
     </h2>
+    <br />
+    <asp:Panel ID="encuestasPanel" runat="server" Text="inicio"></asp:Panel>
+    <asp:Chart ID="Resultado" runat="server" EnableTheming="true"
+        BorderDashStyle="Solid" BackSecondaryColor="White"
+        BackGradientStyle="TopBottom" BorderWidth="2px" BackColor="211, 223, 240"
+        BorderColor="#1A3B69">
+        <Series>
+            <asp:Series Name="Categories" ChartType="Bar" ChartArea="MainChartArea"
+                BorderWidth="2" Color="YellowGreen">
+            </asp:Series>
+        </Series>
 
+        <ChartAreas>
+            <asp:ChartArea Name="MainChartArea" Area3DStyle-Enable3D="true"
+                BorderColor="64, 64, 64, 64"
+                BorderDashStyle="Solid" BackSecondaryColor="White"
+                BackColor="64, 165, 191, 228"
+                ShadowColor="Transparent" BackGradientStyle="TopBottom">
+                <Area3DStyle Rotation="10" Perspective="10" Inclination="15"
+                    IsRightAngleAxes="False" WallWidth="0" IsClustered="False"></Area3DStyle>
+                <AxisY LineColor="64, 64, 64, 64">
+                    <LabelStyle Font="Trebuchet MS, 8.25pt, style=Bold" />
+                    <MajorGrid LineColor="64, 64, 64, 64" />
+                </AxisY>
+                <AxisX LineColor="64, 64, 64, 64">
+                    <LabelStyle Font="Trebuchet MS, 8.25pt, style=Bold" />
+                    <MajorGrid LineColor="64, 64, 64, 64" />
+                </AxisX>
+            </asp:ChartArea>
+        </ChartAreas>
+    </asp:Chart>
+    <br />
+    <br />
+    <asp:Label ID="lblMensajes" runat="server" CssClass="formError"></asp:Label>
+    <br />
+    <div class="clear"></div>
     <br />
 </asp:Content>
--- UI/Pages/ResultadosEncuestas.aspx.vb
+++ UI/Pages/ResultadosEncuestas.aspx.vb
@@ -1,8 +1,65 @@
-﻿Public Class ResultadosEncuestas
-    Inherits System.Web.UI.Page
+﻿Imports EE
+Imports Business
+Imports System.Web.UI.DataVisualization.Charting
+
+Public Class ResultadosEncuestas
+    Inherits PaginaAutorizada
+
+    Dim encuestaBaseBusiness As New FichaEncuestaBase_Business
+    Dim encuestaBusiness As New FichaEncuesta_Business
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        CargarEncuesta()
+        Resultado.Visible = False 'lo uso para copiarlo
     End Sub
 
+    Protected Overrides Sub TraducirComponentesDinamicos()
+    End Sub
+
+    Private Sub CargarEncuesta()
+        encuestasPanel.Controls.Clear()
+
+        For Each encuestaBase In encuestaBaseBusiness.ListarEncuestas
+
+            Dim descripcionInicio = New LiteralControl()
+            descripcionInicio.Text = ""
+            descripcionInicio.Text += "<div class='w3-card-4'>"
+            descripcionInicio.Text += "<header class='w3-container w3-blue'>"
+            descripcionInicio.Text += encuestaBase.Descripcion
+            descripcionInicio.Text += "</header>"
+            descripcionInicio.Text += "<div class='w3-container'>"
+            encuestasPanel.Controls.Add(descripcionInicio)
+
+            'muestro resultados
+
+            Dim resultados As New List(Of EncuestaResultado)
+            For Each opcion In encuestaBase.Preguntas
+                resultados.Add(New EncuestaResultado With {.Opcion = opcion.Pregunta})
+            Next
+            For Each encuesta In encuestaBusiness.Listar
+                For Each encuestaResultado In resultados
+                    If encuesta.RespuestaUnica = encuestaResultado.Opcion Then
+                        encuestaResultado.Votos += 1
+                    End If
+                Next
+            Next
+
+            Dim nuevoResultado As New Chart
+            nuevoResultado.Series.Add("Categories")
+            nuevoResultado.ChartAreas.Add("MainChartArea")
+
+            nuevoResultado.Series("Categories").XValueMember = "Opcion"
+            nuevoResultado.Series("Categories").YValueMembers = "Votos"
+            nuevoResultado.DataSource = resultados
+            nuevoResultado.DataBind()
+            encuestasPanel.Controls.Add(nuevoResultado)
+
+            Dim descripcionCierre = New LiteralControl()
+            descripcionCierre.Text = ""
+            descripcionCierre.Text += "</div>"
+            descripcionCierre.Text += "</div>"
+            descripcionCierre.Text += "<br/><hr/><br/>"
+            encuestasPanel.Controls.Add(descripcionCierre)
+        Next
+    End Sub
 End Class
