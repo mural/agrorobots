f07c083e6976da0204e376759ab5889775e60570
Agustin <mural86@gmail.com>
2015-11-07 02:43:24 -0300

    Filtrar novedades por fecha inicio y fin

--- Business/Servicios/Novedades_Business.vb
+++ Business/Servicios/Novedades_Business.vb
@@ -8,4 +8,15 @@ Public Class Novedades_Business
         Me.mapperGenerico = New Novedades_Mapper
     End Sub
 
+    Public Overrides Function Listar() As List(Of Novedades)
+        Dim listaNovedades As New List(Of Novedades)
+        Dim fechaHoy = Date.Now
+        For Each novedad In mapperGenerico.Listar()
+            If fechaHoy > novedad.FechaInicio And fechaHoy < novedad.FechaFin Then
+                listaNovedades.Add(novedad)
+            End If
+        Next
+        Return listaNovedades
+    End Function
+
 End Class
