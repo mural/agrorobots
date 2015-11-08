1bdd1de995bd3b5c790cef78641f129e31c29130
Agustin <mural86@gmail.com>
2015-11-08 03:31:40 -0300

    Validacion de cupo en inscripcion

--- UI/Pages/CarrerasDetalle.aspx.vb
+++ UI/Pages/CarrerasDetalle.aspx.vb
@@ -54,27 +54,32 @@ Public Class CarrerasDetalle
     Protected Sub inscribirse_Click(sender As Object, e As EventArgs) Handles inscribirse_118.Click
         'comprobar sesion
         If UsuarioLogueado() Then
-            'ver que no este inscripto
-            Dim elementoABuscar As ElementoAcademico = Nothing
-            For Each elemAcademico As ElementoAcademico In usuario.ElementosAcademicos
-                If elemAcademico.CodigoAcademico = elementoAcademico.CodigoAcademico Then
-                    elementoABuscar = elemAcademico
-                    Exit For
+            'hay cupo ?
+            If elementoAcademico.Cupo > 0 Then
+                'ver que no este inscripto
+                Dim elementoABuscar As ElementoAcademico = Nothing
+                For Each elemAcademico As ElementoAcademico In usuario.ElementosAcademicos
+                    If elemAcademico.CodigoAcademico = elementoAcademico.CodigoAcademico Then
+                        elementoABuscar = elemAcademico
+                        Exit For
+                    End If
+                Next
+                If Not elementoABuscar Is Nothing Then
+                    MensajeError(lblMensajes, idiomas.GetTranslationById(183)) 'Ya esta inscripto
+                Else
+                    'agrego al carrito
+                    Dim carritoSesion As List(Of ElementoAcademico) = Session("carrito")
+                    If carritoSesion Is Nothing Then
+                        carritoSesion = New List(Of ElementoAcademico)
+                    End If
+                    If Not carritoSesion.Contains(elementoAcademico) Then
+                        carritoSesion.Add(elementoAcademico)
+                    End If
+                    Session("carrito") = carritoSesion
+                    Response.Redirect(PaginasConocidas.CARRITO)
                 End If
-            Next
-            If Not elementoABuscar Is Nothing Then
-                MensajeError(lblMensajes, idiomas.GetTranslationById(183))
             Else
-                'agrego al carrito
-                Dim carritoSesion As List(Of ElementoAcademico) = Session("carrito")
-                If carritoSesion Is Nothing Then
-                    carritoSesion = New List(Of ElementoAcademico)
-                End If
-                If Not carritoSesion.Contains(elementoAcademico) Then
-                    carritoSesion.Add(elementoAcademico)
-                End If
-                Session("carrito") = carritoSesion
-                Response.Redirect(PaginasConocidas.CARRITO)
+                MensajeError(lblMensajes, idiomas.GetTranslationById(184)) 'No hay cupo
             End If
         Else
             Response.Redirect(PaginasConocidas.LOGIN + "?inscribir=" + idElementoAcademico)
